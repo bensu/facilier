a40199c9100b5f6ba5c1f872a140f2406b4d892b
Sebastian Bensusan <sbensu@gmail.com>
2016-02-09 23:04:36 +0100

    Better icons and polish

--- src/cljs/facilier/panel.cljs
+++ src/cljs/facilier/panel.cljs
@@ -1,8 +1,9 @@
 (ns facilier.panel
   "Application to test and develop Facilier itself"
-  (:require [om.core :as om]
+  (:require [cljs.pprint :as pp :refer [pprint]]
+            [clojure.string :as str]
+            [om.core :as om]
             [sablono.core :as html :refer-macros [html]]
-            [cljs.pprint :as pp :refer [pprint]]
             [ankha.core :as ankha]
             [facilier.client :as f]))
 
@@ -11,12 +12,22 @@
 ;; ======================================================================
 ;; Data
 
+(def info
+  {:browser :chrome
+   :browser-version "42.0.2311.135"
+   :platform :windows
+   :platform-version ""
+   :engine :webkit
+   :engine-version "537.36"
+   :screen [1535 789]
+   :agent "Mozilla/5.0 (X11; Linux x86_64) ..."})
+
 (def sessions
   (let [sessions (take 10 (repeatedly (fn [] {:uuid (str (random-uuid))
                                              :date (js/Date.)
                                              :duration "20 min"
-                                             :platform "Chrome"
-                                             :status "OK"
+                                             :info info
+                                             :status :ok
                                              :state {:some "stuff"}})))]
     (zipmap (map :uuid sessions) sessions)))
 
@@ -30,17 +41,39 @@
 ;; ======================================================================
 ;; HTML
 
-(defn session-view [session owner]
+(defn browser-class [browser]
+  {:pre [(keyword? browser)]}
+  (str "fa icon-cell fa-" (name browser)))
+
+(defn os-class [os]
+  (str "fa icon-cell fa-" (if (= :mac os) "apple" (name os))))
+
+(defn browser-name [info]
+  (str (str/capitalize (name (:browser info))) " " (:version info)))
+
+(defn status-class [status]
+  {:pre [(keyword? status)]}
+  (str "fa icon-cell fa-" (case status :ok "check" :error "ban")))
+
+(defn platform-icons [info]
+  [:span
+   [:i {:class (os-class (:platform info))}]
+   " "
+   [:i {:class (browser-class (:browser info))}]])
+
+(defn session-view [session owner {:keys [quit-fn]}]
   (reify
     om/IRender
     (render [_]
       (html
-       (let [{:keys [uuid date duration platform status]} session]
+       (let [{:keys [uuid date duration info status]} session]
          [:div.session nil
+          [:i.fa.fa-times.u-pull-right {:onClick (fn [_] (quit-fn uuid))}]
           [:p "Session id: " uuid]
+          [:p "Platform: " (browser-name info)]
           [:p "Time: " (str date)]
           [:p "Duration: " duration]
-          [:p "Status: " status]
+          [:p "Status: " [:i {:class (status-class status)}]]
           [:div.state "State:" (om/build ankha/inspector (:state session))]])))))
 
 
@@ -48,17 +81,23 @@
 ;; Table
 
 (def title-row
-  [:tr [:th "Session Id"] [:th "Time"] [:th "Duration"] [:th "Platform"] [:th "Status"]])
+  [:tr
+   [:th.row-left "Session Id"] [:th "Time"] [:th "Duration"]
+   [:th "Platform"] [:th.row-right "Status"]])
 
 (defn row [session owner {:keys [click-fn]}]
   (reify
     om/IRender
     (render [_]
-      (let [{:keys [uuid date duration platform status]} session]
+      (let [{:keys [uuid date duration info status]} session]
         (html
-         [:tr {:onClick (fn [_]
-                        (click-fn uuid))}
-          [:td uuid] [:td (str date)] [:td duration] [:td platform] [:td status]])))))
+         [:tr.session-row {:onClick (fn [_]
+                                      (click-fn uuid))}
+          [:td.row-left uuid]
+          [:td (str date)]
+          [:td.center duration]
+          [:td.center (platform-icons info)]
+          [:td.row-right.center [:i {:class (status-class status)}]]])))))
 
 (defn widget [{:keys [sessions] :as data} owner]
   (reify
@@ -70,14 +109,15 @@
          [:form {}
           [:label {:htmlFor "search"} "Search"]
           [:input#search {:type "search"}]]]
-        [:div.main {}
-         [:table.u-full-width {}
-          [:thead title-row]
-          [:tbody (mapv #(om/build row % {:opts {:click-fn (fn [uuid]
-                                                             (om/update! data :session uuid))}})
-                        (vals (:sessions data)))]]
-         (when-let [session-id (:session data)]
-           (om/build session-view (get sessions session-id)))]]))))
+        (if-let [session-id (:session data)]
+          (om/build session-view (get sessions session-id) {:opts {:quit-fn (fn [_]
+                                                                              (om/update! data :session nil))}})
+          [:div.main {}
+           [:table.session-table.u-full-width {}
+            [:thead title-row]
+            [:tbody (mapv #(om/build row % {:opts {:click-fn (fn [uuid]
+                                                               (om/update! data :session uuid))}})
+                          (vals (:sessions data)))]]])]))))
 
 
 (defn init []
